92dc49165e78c2b2eddb4c4397fa5e89711a7422
Miguel Marin <ElCateto300@gmail.com>
2018-11-06 10:27:59 -0500

    All buttons

--- main.js
+++ main.js
@@ -2,3 +2,20 @@
 function sayHello() {
   console.log("Hello, World!");
 }
+
+function popupHello() {
+  alert("Hello, World!");
+}
+
+function browserHello() {
+  var p=document.getElementById('browserHello')
+  p.innerHTML="Hello, World!";
+}
+
+function strangerHello() {
+  var person = prompt("Please enter your name");
+
+  if (person !=null) {
+      document.getElementById("strangerHello").innerHTML ="Hello " + person + "! How are you today?";
+  }
+}
